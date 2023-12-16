7ceb44cec1e6ffaebdc64fd993384c58000ad72f
Vitalie-Olexici <olexici.dev@gmail.com>
2023-12-16 22:43:51 +0200

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, состоящий из цифр этого числа. 
+//Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. Размер массива должен быть равен количеству цифр.
+
+Random rnd = new Random();
+int value = rnd.Next(1, 100001);
+Console.WriteLine(value);
+
+char[] arr = value.ToString().ToCharArray();
+
+void Main(char[] arr){
+    Array.Sort(arr);
+    Array.Reverse(arr);
+    Console.WriteLine(String.Join(", ", arr));
+}
+
+Main(arr);
