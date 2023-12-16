0c98a0a84f6825af3a7a8f55b8acdd312f939af6
Vitalie-Olexici <olexici.dev@gmail.com>
2023-12-16 22:23:51 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+
+
+void FillArray(int[] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length){
+        collection[index] = new Random().Next();
+        index ++;
+    }
+}
+
+void PrintArray(int [] col){
+    int count = col.Length;
+    int position = 0;
+    while (position < count){
+    Console.Write(col[position] +" ");
+    position++;
+    }
+}
+
+
+void FindNumber(int [] array){
+    int num = 0;
+    for (int i = 0; i < array.Length; i++ ){
+            if (array[i] % 2 == 0) {
+                num++;
+            }
+        }
+    Console.WriteLine();
+    Console.Write(num);
+        
+}
+
+FindNumber(array);
